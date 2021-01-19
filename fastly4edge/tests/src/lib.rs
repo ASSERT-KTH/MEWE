15444afb8a5a7a2bd4931585ce563a6b3a01cff1
Jacarte <javierca@kth.se>
2021-01-19 19:54:31 +0100

    Adding memory emisor

--- fastly4edge/tests/src/lib.rs
+++ fastly4edge/tests/src/lib.rs
@@ -7,7 +7,7 @@ fn testCFG() {
     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 	d.push("resources/babbage.wasm");
 
-	let ret = translate2mir(d.into_os_string().to_str().expect("Not valid str"), "__original_main", "babbage");
+	let ret = translate2mir(d.into_os_string().to_str().expect("Not valid str"), "printf_core", "babbage");
 
     println!("{}", ret)
 }
--- fastly4edge/wat2mir/src/lib.rs
+++ fastly4edge/wat2mir/src/lib.rs
@@ -1,7 +1,7 @@
 #![crate_name = "wat2mir"]
 
-use walrus::{LocalFunction, ModuleFunctions, ModuleLocals, ValType, ir::Visitor};
-use std::path::PathBuf;
+use walrus::*;
+use std::{default, env::args, path::PathBuf};
 use walrus::ir::*;
 use std::collections::HashMap;
 
@@ -13,13 +13,51 @@ enum BLOCKTPE {
 
 struct MIRVisitor<'a> {
     minLocal: i32,
-    locals: &'a walrus::ModuleLocals,
+    module: &'a walrus::Module,
     localFunction: Option<&'a walrus::LocalFunction>,
     blockHash: HashMap<usize, BLOCKTPE>,
-    ret: String
+    ret: String,
+    blockDepth: Vec<(InstrSeqId, i32)>,
+    depth: i32
 }
 
+impl MIRVisitor<'_>{
 
+    fn get_depth(&mut self, instrId: InstrSeqId) -> i32{
+        self.blockDepth.len() as i32 - self.blockDepth.iter()
+        .find(|(r, _)| r == &instrId).expect("Error in DFS traveling").1
+    }
+
+    fn emit_mem_kind_store(&mut self, kind: StoreKind){
+        match kind {
+            StoreKind::I32 { atomic} => self.ret.push_str(&format!("i32.store ")),
+            StoreKind::I64 { atomic} => self.ret.push_str(&format!("i64.store ")),
+            StoreKind::I32_16 { atomic} => self.ret.push_str(&format!("i32.store16 ")),
+            StoreKind::I32_8 { atomic} => self.ret.push_str(&format!("i32.store8 ")),
+            StoreKind::I64_8 { atomic} => self.ret.push_str(&format!("i64.store8 ")),
+            StoreKind::I64_16 { atomic} => self.ret.push_str(&format!("i64.store16 ")),
+            StoreKind::I64_32 { atomic} => self.ret.push_str(&format!("i64.store32 ")),
+            StoreKind::F32 => self.ret.push_str(&format!("f32.store ")),
+            StoreKind::F64  => self.ret.push_str(&format!("f64.store ")),
+            _ => unimplemented!("ERROR {:?}", kind)
+        }
+    }
+
+    fn emit_mem_kind_load(&mut self, kind: LoadKind){
+        match kind {
+            LoadKind::I32 {atomic} => self.ret.push_str(&format!("i32.load ")),
+            LoadKind::I32_8 { kind} => self.ret.push_str(&format!("i32.load8_u ")),
+            LoadKind::I64 { atomic} => self.ret.push_str(&format!("i64.load ")),
+            LoadKind::F64 => self.ret.push_str(&format!("f64.load ")),
+            LoadKind::I64_32 { kind: ZeroExtend } => self.ret.push_str(&format!("i64.load32_u ")),
+            _ => panic!("ERROR {:?}", kind)
+        }
+    }
+
+    fn emit_mem_arg(&mut self, arg: MemArg){
+        self.ret.push_str(&format!("offset={} align={}", arg.offset, arg.align));
+    }
+}
 /// check for the index to start the local operations. To emit the MIR code then, local.idx - minLocal
 struct LocalGatheringVisitor<'a> {
     minLocal: i32,
@@ -74,30 +112,170 @@ impl Visitor<'_> for LocalGatheringVisitor<'_> {
         };
         self.setUsedLocal(instr.local);
     }
+
     
 }
 
 impl Visitor<'_> for MIRVisitor<'_> {
-
-    fn visit_call(&mut self, instr: &walrus::ir::Call){
-        
+    
+    fn visit_instr_seq_id(&mut self, instr: &InstrSeqId){ 
+        //self.ret.push_str(&format!("{:?}\n", instr))
+    }
+    
+    fn visit_local_id(&mut self, instr: &LocalId){ 
+        self.ret.push_str(& format!("{}\n", (instr.index() as i32) - self.minLocal));
     }
 
+    fn visit_memory_id(&mut self, instr: &MemoryId){ 
+        // TODO assume that memoryId is always 0
+        // Check ffmpeg example to check
+        //unimplemented!("Not implemented visit_memory_id {}", self.ret) 
+        self.ret.push_str(&format!("\n"))
+    }
+
+    fn visit_table_id(&mut self, instr: &TableId){ unimplemented!("Not implemented visit_table_id {}", self.ret) }
+
+    fn visit_global_id(&mut self, instr: &GlobalId){ 
+        self.ret.push_str(& format!("{:?}\n", self.module.globals.get(*instr).id().index()))
+    }
+
+    fn visit_function_id(&mut self, instr: &FunctionId){ 
+        self.ret.push_str(&format!("{:?} \n", instr))
+    }
+
+    fn visit_data_id(&mut self, instr: &DataId){ 
+        unimplemented!("Not implemented visit_data_id {}", self.ret) 
+    }
+
+    //fn visit_type_id(&mut self, instr: &TypeId){ unimplemented!("Not implemented visit_type_id {}", self.ret) }
+        
+    //fn visit_value(&mut self, instr: &Value){ unimplemented!("Not implemented visit_value {}", self.ret) }
+
+    fn visit_call(&mut self, instr: &Call){ 
+        self.ret.push_str(&format!("call "))
+    }
+
+    fn visit_call_indirect(&mut self, instr: &CallIndirect){ unimplemented!("Not implemented visit_call_indirect {}", self.ret) }
+
+    fn visit_local_tee(&mut self, instr: &LocalTee){ 
+
+        self.ret.push_str(&format!("local.tee "))
+    }
+        
+    fn visit_global_set(&mut self, instr: &GlobalSet){ 
+        self.ret.push_str(&format!("global.set "))
+    }
+        
+    fn visit_binop(&mut self, instr: &Binop){ 
+        //TODO
+        self.ret.push_str(&format!("{:?}\n", instr.op))
+    }
+
+    fn visit_unop(&mut self, instr: &Unop){ 
+        //TODO
+        self.ret.push_str(&format!("{:?}\n", instr.op))
+    }
+
+    fn visit_select(&mut self, instr: &Select){ 
+        self.ret.push_str("select")
+    }
+
+    fn visit_unreachable(&mut self, instr: &Unreachable){ 
+        self.ret.push_str("unreachable")
+    }
+
+    fn visit_br(&mut self, instr: &Br){ 
+        let blockid = instr.block;
+
+        let depth = self.blockDepth.len() as i32 - self.blockDepth.iter()
+        .find(|(r, _)| r == &blockid).expect("Error in DFS traveling").1;
+        self.ret.push_str(&format!("br {:?}\n", depth))
+    }
+
+    fn visit_br_if(&mut self, instr: &BrIf){ 
+        let blockid = instr.block;
+        let depth = self.get_depth(blockid);
+        self.ret.push_str(&format!("br_if {:?}\n", depth))
+    }
+        
+    fn visit_br_table(&mut self, instr: &BrTable){ 
+        let id1= self.get_depth(instr.default);
+
+        let remaining = instr.blocks.iter().map(|&s| {format!(" {:?}", self.get_depth(s))})
+        .collect::<Vec<_>>().join(" ");
+
+        self.ret.push_str(&format!("br_table {} {}\n", remaining, id1))
+    }
+
+    fn visit_drop(&mut self, instr: &Drop){ 
+        self.ret.push_str("drop\n")
+    }
+
+    fn visit_return(&mut self, instr: &Return){ unimplemented!("Not implemented visit_return {}", self.ret) }
+
+    fn visit_memory_size(&mut self, instr: &MemorySize){ unimplemented!("Not implemented visit_memory_size {}", self.ret) }
+
+    fn visit_memory_grow(&mut self, instr: &MemoryGrow){ unimplemented!("Not implemented visit_memory_grow {}", self.ret) }
+
+    fn visit_memory_init(&mut self, instr: &MemoryInit){ unimplemented!("Not implemented visit_memory_init {}", self.ret) }
+
+    fn visit_data_drop(&mut self, instr: &DataDrop){ unimplemented!("Not implemented visit_data_drop {}", self.ret) }
+
+    fn visit_memory_copy(&mut self, instr: &MemoryCopy){ unimplemented!("Not implemented visit_memory_copy {}", self.ret) }
+
+    fn visit_memory_fill(&mut self, instr: &MemoryFill){ unimplemented!("Not implemented visit_memory_fill {}", self.ret) }
+
+    fn visit_load(&mut self, instr: &Load){ 
+        
+        self.emit_mem_kind_load(instr.kind);
+        self.emit_mem_arg(instr.arg);
+    }
+
+    fn visit_store(&mut self, instr: &Store){ 
+
+        self.emit_mem_kind_store(instr.kind);
+        self.emit_mem_arg(instr.arg);
+        //unimplemented!("Not implemented visit_store {}", self.ret)
+     }
+
+    fn visit_atomic_rmw(&mut self, instr: &AtomicRmw){ unimplemented!("Not implemented visit_atomic_rmw {}", self.ret) }
+
+    fn visit_cmpxchg(&mut self, instr: &Cmpxchg){ unimplemented!("Not implemented visit_cmpxchg {}", self.ret) }
+
+    fn visit_atomic_notify(&mut self, instr: &AtomicNotify){ unimplemented!("Not implemented visit_atomic_notify {}", self.ret) }
+
+    fn visit_atomic_wait(&mut self, instr: &AtomicWait){ unimplemented!("Not implemented visit_atomic_wait {}", self.ret) }
+
+    fn visit_atomic_fence(&mut self, instr: &AtomicFence){ unimplemented!("Not implemented visit_atomic_fence {}", self.ret) }
+
+    fn visit_table_get(&mut self, instr: &TableGet){ unimplemented!("Not implemented visit_table_get {}", self.ret) }
+
+    fn visit_table_set(&mut self, instr: &TableSet){ unimplemented!("Not implemented visit_table_set {}", self.ret) }
+
+    fn visit_table_grow(&mut self, instr: &TableGrow){ unimplemented!("Not implemented visit_table_grow {}", self.ret) }
+
+    fn visit_table_size(&mut self, instr: &TableSize){ unimplemented!("Not implemented visit_table_size {}", self.ret) }  
+            
+
+    fn visit_v128_swizzle(&mut self, instr: &V128Swizzle){ unimplemented!("Not implemented visit_v128_swizzle {}", self.ret) }
+
+
+    fn visit_v128_shuffle(&mut self, instr: &V128Shuffle){ unimplemented!("Not implemented visit_v128_shuffle {}", self.ret) }
 
     fn visit_local_get(&mut self, instr: &walrus::ir::LocalGet)
     {   
-        self.ret.push_str(& format!("local.get {}\n", (instr.local.index() as i32) - self.minLocal));
+        self.ret.push_str(& format!("local.get "));
     }
 
 
     fn visit_global_get(&mut self, instr: &walrus::ir::GlobalGet)
     {   
-        //println!("global.get {:?}", (instr.global.index() as i32) - self.minLocal);
+        self.ret.push_str(& format!("global.get "));
     }
 
     fn visit_local_set(&mut self, instr: &walrus::ir::LocalSet)
     {  
-        self.ret.push_str(& format!("local.set {}\n", (instr.local.index() as i32) - self.minLocal));
+        self.ret.push_str(& format!("local.set "));
     }
 
 
@@ -126,6 +304,12 @@ impl Visitor<'_> for MIRVisitor<'_> {
         self.ret.push_str("if\n");
     }
 
+    fn start_instr_seq(&mut self, instr_seq: &InstrSeq)
+    {
+        self.depth += 1;
+        self.blockDepth.push((instr_seq.id(), self.depth))
+    }
+
     fn end_instr_seq(&mut self, instr_seq: &InstrSeq)
     {
         let tpe = self.blockHash.get(&instr_seq.id().index());
@@ -138,6 +322,9 @@ impl Visitor<'_> for MIRVisitor<'_> {
                 _ => println!("Unknown")
             }
         }
+        let id = instr_seq.id();
+        self.depth -= 1;
+        self.blockDepth.pop();
     }
 }    
 
@@ -154,7 +341,9 @@ pub fn translate2mir(file_name: &str, func_name: &str, as_function: &str) -> Str
    let mirVisitor = &mut MIRVisitor{ 
        blockHash:HashMap::new(), 
         localFunction: None,
-        locals: &module.locals, minLocal: 0, ret:  String::from("") };
+        depth: 0,
+        blockDepth:vec![],
+        module: &module, minLocal: 0, ret:  String::from("") };
 
    module.funcs.iter_local().for_each(|(fromFuncId, item )| {
 
@@ -178,7 +367,7 @@ pub fn translate2mir(file_name: &str, func_name: &str, as_function: &str) -> Str
         walrus::ir::dfs_in_order(localVisitor, item, item.entry_block());
 
         let locals = &localVisitor.usedLocals.iter().map(|(_k, val)| {
-            format!(" {:?}", val).to_lowercase()
+            format!(" {:?}", &val).to_lowercase()
         }).collect::<Vec<_>>();
 
         let args = &item.args.iter().map(|t| {
